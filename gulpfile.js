525d219be8811a0595cd94b2f9507e6862a2570f
Demian Borba <demian.borba@actioncreations.com>
2014-03-26 00:42:54 -0300

    Adding SnapJS basic functionaliy, Bower and Gulp edits

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,45 @@
 var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     jshint = require('gulp-jshint'),
-    stylish = require('jshint-stylish');
+    stylish = require('jshint-stylish'),
     rename = require('gulp-rename'),
     concat = require('gulp-concat');
 
-gulp.task('lint', function() {
-  gulp.src('src/js/*.js')
-    .pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish'));
+gulp.task('lint', function () {
+
+    gulp.src('src/js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'));
+
 });
 
-gulp.task('scripts', function() {
-  gulp.src('src/js/*.js')
-    .pipe(concat('all.js'))
-    .pipe(uglify({
+gulp.task('scripts', function () {
+    
+    // load only necessary files to dist
+    gulp.src('src/bower_components/snapjs/snap.min.js')
+        .pipe(gulp.dest('dist/bower_components/snapjs/'))
+    gulp.src('src/bower_components/snapjs/snap.css')
+        .pipe(gulp.dest('dist/bower_components/snapjs/'))
+    
+    // load assets
+    gulp.src('src/assets/*')
+        .pipe(gulp.dest('dist/assets/'))
+    
+    // uglify and load JS
+    gulp.src('src/js/*.js')
+        .pipe(uglify({
             outSourceMap: true
         }))
-    .pipe(rename("all-min.js"))
-    .pipe(gulp.dest('dist/js/'))
+        .pipe(gulp.dest('dist/js/'))
+    
+    // load CSS
+    gulp.src('src/css/*.css')
+        .pipe(gulp.dest('dist/css/'))
+    
+    // uglify and load all html files to dist
+    gulp.src('src/*.html')
+        .pipe(gulp.dest('dist/'))
+
 });
 
 gulp.task('default', ['lint', 'scripts']);
