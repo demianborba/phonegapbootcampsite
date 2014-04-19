80343c57a1682b1dc2f8cffe5760b06dcbc40389
Demian Borba <demian.borba@actioncreations.com>
2014-04-19 12:42:51 -0300

    Finish Target Audience Page

--- src/js/controller/TargetController.js
+++ src/js/controller/TargetController.js
@@ -7,6 +7,10 @@ app.controller('TargetController', ['$scope', 'AppEventManager',
             "title": {
                 "ptLabel": "Público<br>Alvo",
                 "enLabel": "Target<br>Audience"
+            },
+            "description": {
+                "ptLabel": "O PhoneGap Bootcamp se destina a Front End Developers, Back End Developers, UX Developers, programadores em geral, Designers com noções de programação, estudantes e profissionais de tecnologia interessados em explorar o mundo de aplicativos móveis, focando em performance e custo benefício no desenvolvimento.",
+                "enLabel": "PhoneGap Bootcamp is targeted to Front End Developers, Back End Developers, UX Developers, programmers in general, Designers with basic programming knowledge, students and technology professionals interested in exploring the world of mobile applications, focused on performance and development cost benefits."
             }
         };
 }]);
