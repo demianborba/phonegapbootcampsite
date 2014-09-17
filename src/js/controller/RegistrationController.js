64bb11d45e0cd37ca9093d683383166d9fdaba0e
Demian Borba <demian.borba@actioncreations.com>
2014-09-17 15:37:29 -0300

    add extra class sao paulo

--- src/js/controller/RegistrationController.js
+++ src/js/controller/RegistrationController.js
@@ -22,11 +22,18 @@ app.controller('RegistrationController', ['$scope', 'AppEventManager', '$rootSco
             },
             "bootcamps": [{
                 "city": "São Paulo - SP - Brasil",
+                "address": "Centro Empresarial Nações Unidas - CENU. Av das Nações Unidas, 12.901. 35º andar – Torre Norte - Brooklin Novo. CEP: 04578-000",
+                "ptDates": "27 e 28 / Setembro / 2014",
+                "enDates": "27 & 28 / September / 2014",
+                "name": "PhoneGap<br>Bootcamp<br>São Paulo FDS",
+                "url": "https://actioncreations.com/store/produto/phonegap-bootcamp-sao-paulo-27-28-setembro-turma-fds/"
+                },{
+                "city": "São Paulo - SP - Brasil",
                 "address": "Espaço iMasters - Rua Claudio Soares, 72 conj 1302 - Próximo ao metrô Faria Lima",
                 "ptDates": "30/Setembro e 1/Outubro, 2014",
                 "enDates": "30/September & 1/October, 2014",
-                "name": "PhoneGap<br>Bootcamp<br>São Paulo",
-                "url": "https://actioncreations.com/store/produto/phonegap-bootcamp-sao-paulo-30-setembro-2014/"
+                "name": "PhoneGap<br>Bootcamp<br>São Paulo<br><span class='red'>vagas esgotadas</span>",
+                "url": "message:Vagas esgotadas para esta turma!"
                 }]
         };
 
